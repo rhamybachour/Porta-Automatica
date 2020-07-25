06ae032d76b56111099213bd84fc2ea24f8bb173
carolinemendg <62621237+carolinemendg@users.noreply.github.com>
2020-07-25 15:43:37 -0300

    Update porta.py

--- porta.py
+++ porta.py
@@ -80,9 +80,96 @@ class Porta:
     Classe responsável pela definição do objeto porta
     """
 
-    pass
+    def __init__(self, calendario, motorEsquerdo, motorDireito, sensorEntrada, sensorSaida):
+        self.calendario = calendario
+        self.sensorPortaEntrada = sensorSaida
+        self.sensorPortaSaida = sensorEntrada
+        self.motorPortaEsquerda = motorEsquerdo
+        self.motorPortaDireita = motorDireito
+        
+        #Fechada = False / Aberta = True
+        self.__estadoPorta = False
+        
+        #Tempo em segundos
+        self.__tempoEsperaPorta = 5
+        self.__tempoAberturaPorta = 3
+        self.__tempoFechamentoPorta = 3
+        
+
+    def detectaPessoa(self):
+
+        #Se os sensores detectaram pessoas saindo ou entrando
+        if self.sensorPortaEntrada.getEstado() == True or self.sensorPortaSaida.getEstado() == True:
+
+            #Verifica se a porta esta fechada
+            if self.__estadoPorta == False:
+                self.abrePorta()
+
+            #Verifica se não chega outra pessoa durante o tempo de espera   
+            self.aguardaPorta()
+
+            #Atualiza os sensores
+            if self.sensorPortaEntrada.getEstado() == True:
+                self.sensorPortaEntrada.setEstado(False)
+
+            if self.sensorPortaSaida.getEstado() == True:
+                self.sensorPortaSaida.setEstado(False)
+                
+    def aguardaPorta(self):
+
+        tempo = 0
+        tempoExtra = 0
+        
+        #Avança o tempo com a porta aberta    
+        print(Cores.YELLOW + "[%d] AGUARDANDO SENSORES" % calendario.getTempoReal(), end = '')
+        time.sleep(1)
+        print(".", end = '')        
+        time.sleep(0.5)
+        print(".", end = '')        
+        time.sleep(0.5)
+        print(".\n\n")
+        time.sleep(1)
+
+        #Gera a chegada de uma pessoa durante o tempo de espera de forma aleatória
+        while tempo < self.__tempoEsperaPorta + tempoExtra:
+            #Probabilidade de outra pessoa passar na porta a cada segundo
+            if random.randint(1,20) == 1:
+                tempoExtra = tempo
+                self.calendario.setTempoReal(calendario.getTempoReal() + tempo)
+                
+                print(Cores.YELLOW + "[%d] OPA CHEGOU OUTRA PESSOA!" % calendario.getTempoReal())
+                print(Cores.YELLOW + "[%d] AGUARDANDO SENSORES" % calendario.getTempoReal(), end = '')
+                time.sleep(1)
+                print(".", end = '')        
+                time.sleep(0.5)
+                print(".", end = '')        
+                time.sleep(0.5)
+                print(".\n\n")
+                time.sleep(1)
 
 
+            tempo += 1
+
+        #Atualiza o tempo total decorrido
+        self.calendario.setTempoReal(calendario.getTempoReal() + self.__tempoEsperaPorta)
+
+
+    def verificaPessoa(self):
+        
+        #Verifica se ainda há presença de pessoas na porta
+        if self.sensorPortaEntrada.getEstado() == False and self.sensorPortaSaida.getEstado()  == False:
+
+        #Verifica se a porta esta aberta
+            if self.__estadoPorta == True:
+                self.fechaPorta()
+	
+    def abrePorta(self):	
+		pass
+	
+    def fechaPorta(self):
+		pass
+	
+		
 ################## Simulação ###############################
 if __name__ == '__main__':
 
